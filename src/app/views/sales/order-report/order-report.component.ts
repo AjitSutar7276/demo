81194aeaf4281594ce283a906d809ca0023a63b2
AjitSutar7276 <infobithunter@gmail.com>
2020-01-20 09:06:29 +0530

    order Print

--- src/app/views/sales/order-report/order-report.component.ts
+++ src/app/views/sales/order-report/order-report.component.ts
@@ -16,7 +16,7 @@ export class OrderReportComponent implements OnInit {
   packingChr: any = 0;
   transport : any = 0;
   finalCost : any = 0;
-
+  output    : any;
   constructor(private route: ActivatedRoute,private orderSerives : OrderReportService) 
   {
         //GET parameter in url below code use
@@ -38,7 +38,8 @@ export class OrderReportComponent implements OnInit {
         })
 
         totalF = this.totalCost + this.packingChr + this.transport + this.totalCGST + this.totalSGST;
-        this.finalCost = totalF.toFixed(2);
+        this.finalCost = Math.round(totalF);
+        this.output = this.price_in_words(this.finalCost);
       },err=>{
         console.log(err);
       })
@@ -58,6 +59,61 @@ export class OrderReportComponent implements OnInit {
     window.print();
 
     document.body.innerHTML = originalContents;
+
+}
+
+public price_in_words(price) {
+  var sglDigit = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"],
+    dblDigit = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"],
+    tensPlace = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"],
+    handle_tens = function(dgt, prevDgt) {
+      return 0 == dgt ? "" : " " + (1 == dgt ? dblDigit[prevDgt] : tensPlace[dgt])
+    },
+    handle_utlc = function(dgt, nxtDgt, denom) {
+      return (0 != dgt && 1 != nxtDgt ? " " + sglDigit[dgt] : "") + (0 != nxtDgt || dgt > 0 ? " " + denom : "")
+    };
+
+  var str = "",
+    digitIdx = 0,
+    digit = 0,
+    nxtDigit = 0,
+    words = [];
+  if (price += "", isNaN(parseInt(price))) str = "";
+  else if (parseInt(price) > 0 && price.length <= 10) {
+    for (digitIdx = price.length - 1; digitIdx >= 0; digitIdx--) switch (digit = price[digitIdx] - 0, nxtDigit = digitIdx > 0 ? price[digitIdx - 1] - 0 : 0, price.length - digitIdx - 1) {
+      case 0:
+        words.push(handle_utlc(digit, nxtDigit, ""));
+        break;
+      case 1:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 2:
+        words.push(0 != digit ? " " + sglDigit[digit] + " Hundred" + (0 != price[digitIdx + 1] && 0 != price[digitIdx + 2] ? " and" : "") : "");
+        break;
+      case 3:
+        words.push(handle_utlc(digit, nxtDigit, "Thousand"));
+        break;
+      case 4:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 5:
+        words.push(handle_utlc(digit, nxtDigit, "Lakh"));
+        break;
+      case 6:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 7:
+        words.push(handle_utlc(digit, nxtDigit, "Crore"));
+        break;
+      case 8:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 9:
+        words.push(0 != digit ? " " + sglDigit[digit] + " Hundred" + (0 != price[digitIdx + 1] || 0 != price[digitIdx + 2] ? " and" : " Crore") : "")
+    }
+    str = words.reverse().join("")
+  } else str = "";
+  return str
 
 }
 }
--- src/app/views/sales/quotation-report/quotation-report.component.ts
+++ src/app/views/sales/quotation-report/quotation-report.component.ts
@@ -17,6 +17,8 @@ export class QuotationReportComponent implements OnInit {
   id : any;
   QuoData = [];
   QuoTotal = 0;
+  output : any;
+  totalData : any;
   exportAsConfig: ExportAsConfig = {
     type: 'pdf', // the type you want to download
     elementId: 'myTableIdElementId', // the id of html/table element,
@@ -45,8 +47,12 @@ export class QuotationReportComponent implements OnInit {
     addition = ele.rate * ele.qty;
     this.QuoTotal = this.QuoTotal + addition;
  })
- console.log('total');
-console.log(this.QuoTotal);
+
+  console.log('total');
+  console.log(this.QuoTotal + this.QuoData[0].pack_amt + this.QuoData[0].transport_amt);
+  this.totalData = this.QuoTotal + this.QuoData[0].pack_amt + this.QuoData[0].transport_amt;
+  this.output = this.price_in_words(Math.round(this.totalData));
+  console.log(this.output);
    },err=>{
 
    })
@@ -72,7 +78,60 @@ console.log(this.QuoTotal);
     document.body.innerHTML = originalContents;
 
 }
+ public price_in_words(price) {
+  var sglDigit = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"],
+    dblDigit = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"],
+    tensPlace = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"],
+    handle_tens = function(dgt, prevDgt) {
+      return 0 == dgt ? "" : " " + (1 == dgt ? dblDigit[prevDgt] : tensPlace[dgt])
+    },
+    handle_utlc = function(dgt, nxtDgt, denom) {
+      return (0 != dgt && 1 != nxtDgt ? " " + sglDigit[dgt] : "") + (0 != nxtDgt || dgt > 0 ? " " + denom : "")
+    };
 
+  var str = "",
+    digitIdx = 0,
+    digit = 0,
+    nxtDigit = 0,
+    words = [];
+  if (price += "", isNaN(parseInt(price))) str = "";
+  else if (parseInt(price) > 0 && price.length <= 10) {
+    for (digitIdx = price.length - 1; digitIdx >= 0; digitIdx--) switch (digit = price[digitIdx] - 0, nxtDigit = digitIdx > 0 ? price[digitIdx - 1] - 0 : 0, price.length - digitIdx - 1) {
+      case 0:
+        words.push(handle_utlc(digit, nxtDigit, ""));
+        break;
+      case 1:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 2:
+        words.push(0 != digit ? " " + sglDigit[digit] + " Hundred" + (0 != price[digitIdx + 1] && 0 != price[digitIdx + 2] ? " and" : "") : "");
+        break;
+      case 3:
+        words.push(handle_utlc(digit, nxtDigit, "Thousand"));
+        break;
+      case 4:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 5:
+        words.push(handle_utlc(digit, nxtDigit, "Lakh"));
+        break;
+      case 6:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 7:
+        words.push(handle_utlc(digit, nxtDigit, "Crore"));
+        break;
+      case 8:
+        words.push(handle_tens(digit, price[digitIdx + 1]));
+        break;
+      case 9:
+        words.push(0 != digit ? " " + sglDigit[digit] + " Hundred" + (0 != price[digitIdx + 1] || 0 != price[digitIdx + 2] ? " and" : " Crore") : "")
+    }
+    str = words.reverse().join("")
+  } else str = "";
+  return str
+
+}
 
 
 
